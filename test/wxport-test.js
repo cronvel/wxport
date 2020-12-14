e20e5e9f0a808fdc439946c2ab18b7a534df5a1d
Cédric Ronvel <cedric.ronvel@gmail.com>
2020-12-14 12:21:25 +0100

    wip: expression parser

--- test/wxport-test.js
+++ test/wxport-test.js
@@ -28,7 +28,154 @@
 
 
 
-describe( "..." , () => {
-	it( "..." ) ;
+const WXport = require( '..' ) ;
+const Expression = WXport.Expression ;
+const Schema = WXport.Schema ;
+
+
+
+describe( "Expression parser" , () => {
+	it( "Parse a selector-only expression" , () => {
+		var expression ;
+
+		expression = Expression.parse( "h1" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'h1', transform: [] } ) ;
+
+		expression = Expression.parse( "   h1   " ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'h1', transform: [] } ) ;
+
+		expression = Expression.parse( "div.content h1" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [] } ) ;
+	} ) ;
+
+	it( "Parse an expression with a transform without argument" , () => {
+		var expression ;
+
+		expression = Expression.parse( "h1 | toLowerCase" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'h1', transform: [ { fn: "toLowerCase" , args: [] } ] } ) ;
+
+		expression = Expression.parse( "   h1   |  toLowerCase  " ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'h1', transform: [ { fn: "toLowerCase" , args: [] } ] } ) ;
+
+		expression = Expression.parse( "div.content h1 | toLowerCase" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "toLowerCase" , args: [] } ] } ) ;
+
+		expect( () => Expression.parse( "div.content h1 | toLowerCase trash" ) ).to.throw.a( SyntaxError ) ;
+	} ) ;
+
+	it( "Parse an expression with a transform with one argument" , () => {
+		var expression ;
+
+		// Using single quote
+		expression = Expression.parse( "div.content h1 | attr( 'src' )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "attr" , args: [ 'src' ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | attr('src')" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "attr" , args: [ 'src' ] } ] } ) ;
+
+		// Using double quote
+		expression = Expression.parse( 'div.content h1 | attr( "src" )' ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "attr" , args: [ 'src' ] } ] } ) ;
+		expression = Expression.parse( 'div.content h1 | attr("src")' ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "attr" , args: [ 'src' ] } ] } ) ;
+
+		// Unquoted
+		expression = Expression.parse( "div.content h1 | attr( src )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "attr" , args: [ 'src' ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | attr(src)" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "attr" , args: [ 'src' ] } ] } ) ;
+
+		// number
+		expression = Expression.parse( "div.content h1 | someFunc( 123 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 123 ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc( -123 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ -123 ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc( -123.456 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ -123.456 ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc( 0.123 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 0.123 ] } ] } ) ;
+
+		// constant
+		expression = Expression.parse( "div.content h1 | someFunc( true )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ true ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc( false )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ false ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc( null )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ null ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc( undefined )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ undefined ] } ] } ) ;
+	} ) ;
+
+	it( "Parse an expression with a transform with multiple arguments" , () => {
+		var expression ;
+
+		// Using single quote
+		expression = Expression.parse( "div.content h1 | someFunc( 'arg1' , 'arg2' )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc('arg1','arg2')" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' ] } ] } ) ;
+
+		// Using double quote
+		expression = Expression.parse( 'div.content h1 | someFunc( "arg1" , "arg2" )' ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' ] } ] } ) ;
+		expression = Expression.parse( 'div.content h1 | someFunc("arg1","arg2")' ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' ] } ] } ) ;
+
+		// Unquoted
+		expression = Expression.parse( "div.content h1 | someFunc( arg1 , arg2 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc(arg1,arg2)" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' ] } ] } ) ;
+
+		// Using mixed quotes
+		expression = Expression.parse( "div.content h1 | someFunc(  'arg1' , \"arg2\" ,  arg3  )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' , 'arg3' ] } ] } ) ;
+		expression = Expression.parse( "div.content h1 | someFunc('arg1',\"arg2\",arg3)" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' , 'arg3' ] } ] } ) ;
+
+		// Using mixed type of arguments
+		expression = Expression.parse( "div.content h1 | someFunc(  'arg1' , \"arg2\" , true , 123.456 ,  arg3 , null , -0.123 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [ { fn: "someFunc" , args: [ 'arg1' , 'arg2' , true , 123.456 , 'arg3' , null , -0.123 ] } ] } ) ;
+	} ) ;
+
+	it( "Parse an expression with multiple transforms with multiple arguments" , () => {
+		var expression ;
+
+		// Using mixed type of arguments
+		expression = Expression.parse( "div.content h1 | someFunc(  'arg1' , \"arg2\" , true ) | someOtherFunc( 123.456 ,  arg3 , null , -0.123 )" ) ;
+		expect( expression ).to.be.an( Expression ) ;
+		expect( expression ).to.be.like( { selector: 'div.content h1', transform: [
+			{ fn: "someFunc" , args: [ 'arg1' , 'arg2' , true ] } ,
+			{ fn: "someOtherFunc" , args: [ 123.456 , 'arg3' , null , -0.123 ] }
+		] } ) ;
+	} ) ;
 } ) ;
 
